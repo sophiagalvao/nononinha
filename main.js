1c82c9994ce7d36d3b55710c2298ded2fa40f2ac
sophiagalvao <162718664+sophiagalvao@users.noreply.github.com>
2024-03-12 11:24:23 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://i.pinimg.com/564x/bb/9b/ba/bb9bbaa6d5aa5280722a0f8682f1fd7a.jpg"
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://conteudo.imguol.com.br/c/entretenimento/93/2023/08/08/rapunzel-tem-uma-historia-do-seculo-x-como-inspiracao-1691530753314_v2_1x1.jpg"
+}
